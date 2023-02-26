f7d65ab493801f959645068f981f0463f730fd45
KienTuongNguyen <kientuongnguyen27697@gmail.com>
2023-02-26 15:53:20 +0700

    Initial Commit

---
+++ FacePartsEditor/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FacePartsEditor
+//
+//  Created by Nguyễn Kiến Tường on 26/02/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FacePartsEditor/FacePartsEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  FacePartsEditorApp.swift
+//  FacePartsEditor
+//
+//  Created by Nguyễn Kiến Tường on 26/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct FacePartsEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
