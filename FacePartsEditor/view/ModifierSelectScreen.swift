1bee8336ac8879c8c9a22b56999ca9694da84e26
KienTuongNguyen <kientuongnguyen27697@gmail.com>
2023-06-25 16:54:16 +0700

    update ui

--- FacePartsEditor/view/ModifierSelectScreen.swift
+++ FacePartsEditor/view/ModifierSelectScreen.swift
@@ -2,12 +2,39 @@
 //  ModifierSelectScreen.swift
 //  FacePartsEditor
 //
-//  Created by Nguyễn Kiến Tường on 24/05/2023.
+//  Created by Tran Cao Khanh Ngoc on 24/05/2023.
 //
 
 import SwiftUI
 
-let SIZE = 180.0
+let SIZE = 250.0
+extension Color {
+  init(_ hex: UInt, alpha: Double = 1) {
+    self.init(
+      .sRGB,
+      red: Double((hex >> 16) & 0xFF) / 255,
+      green: Double((hex >> 8 ) & 0xFF) / 255,
+      blue: Double(hex & 0xFF) / 255,
+      opacity: alpha
+    )
+  }
+}
+
+struct CustomToggleStyle: ToggleStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        Button(action: { configuration.isOn.toggle() }) {
+            Label(
+                title: { configuration.label },
+                icon: {}
+            )
+        }
+        .foregroundColor(configuration.isOn ? .white : .purple)
+        .frame(width: 60)
+        .padding(.vertical, 4)
+        .background(configuration.isOn ? Color.pink.opacity(0.4) : Color.clear)
+        .cornerRadius(5)
+    }
+}
 
 struct ModifierSelectScreen: View {
     var inputImage: UIImage
@@ -15,44 +42,110 @@ struct ModifierSelectScreen: View {
     @State var inputDescription: String = "A human face"
     @State var outputDescription: String = ""
     @State var outputImage: UIImage?
+    @State var showsResult = false
     
-    @State var selectedList: [Bool] = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false]
-    var selection = ["background", "face", "lb", "rb", "le", "re", "nose","ulip", "llip", "imouth", "hair", "lr", "rr", "neck", "cloth", "eyeg", "hat", "earr"]
+    @State var processing = false
     
+    @State var selectedList: [Bool] = [false, false, false, false, false, false, false, false, false, false]
+    @State var test: [[Int]] = [[1], [2], [3], [4], [5], [6], [7], [8], [9], [10]]
+    var selection = ["face", "lb", "rb", "le", "re", "nose","ulip", "llip", "imouth", "hair"]
+//    var selection = ["Face", "eyes brown", "eyes", "nose","mouth", "hair"]
+//    var selectionImage = ["face.smiling", "eyebrow", "eye", "nose","mouth", "hair"]
+
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 0) {
-            // Images
-            HStack {
-                inputImageView
-                Spacer()
-                outputImageView
-            }
-            .frame(maxWidth: .infinity, alignment: .top)
-            .padding(.bottom, 32)
-            
-            // Description
-            Text("Output image description")
-            TextField("Enter description for output image", text: $outputDescription)
-                .textFieldStyle(.roundedBorder)
-                .padding(.bottom, 32)
-
-            // Parts selection
-            Text("Select face parts to modify")
-            LazyVGrid(columns: [GridItem(), GridItem(), GridItem(), GridItem()]) {
-                ForEach(Array($selectedList.enumerated()), id: \.offset) {id, $isOn in
-                    Toggle(isOn: $isOn) {
-                        Text(selection[id])
-                    }
-                    .toggleStyle(ButtonToggleStyle())
+        ZStack {
+            VStack(alignment: .leading, spacing: 0) {
+                // Images
+                HStack {
+                    inputImageView
+                    Spacer()
+    //                outputImageView
+                    inputSelectPart
                 }
+                .frame(maxWidth: .infinity, alignment: .top)
+                .padding(.bottom, 32)
+                
+                // Description
+                Text("Input image description")
+                    .padding(.bottom, 10)
+                    .bold()
+                    .foregroundColor(Color.purple.opacity(0.9))
+                TextField("Enter description for input image", text: $inputDescription)
+                    .textFieldStyle(.roundedBorder)
+                    .padding(.bottom, 16)
+                
+                Text("Output image description")
+                    .padding(.bottom, 10)
+                    .padding(.top, 20)
+                    .foregroundColor(Color.purple.opacity(0.9))
+                    .bold()
+                
+                TextField("Enter description for output image", text: $outputDescription)
+                    .textFieldStyle(.roundedBorder)
+                    .padding(.bottom, 32)
+
+                // Parts selection
+    //            Text("Select face parts to modify")
+    //                .padding(.bottom, 10)
+    //                .foregroundColor(Color.purple.opacity(0.9))
+    //            LazyVGrid(columns: [GridItem(), GridItem(), GridItem(), GridItem()]) {
+    //                ForEach(Array($selectedList.enumerated()), id: \.offset) {id, $isOn in
+    //                    Toggle(isOn: $isOn) {
+    //                        Text(selection[id])
+    ////                        Image(systemName: selectionImage[id])
+    //                    }
+    //                    .toggleStyle(CustomToggleStyle())
+    //                }
+    //            }
+    //            .frame(maxWidth: .infinity)
+    //            .background(Color(0xe4aadf).opacity(0.6))
+    //            .cornerRadius(8)
+    //            .padding(.bottom, 32)
+                
+                
+
+
+
+                // Action Button
+                VStack(alignment: .center) {
+                    actionButton
+                }
+                .frame(maxWidth: .infinity, alignment: .center)
             }
-            .background(Color.pink.opacity(0.6))
-            .cornerRadius(8)
+            .padding(20)
+
+            if (processing) {
+                ProgressView()
+                    .progressViewStyle(CircularProgressViewStyle())
+                    .scaleEffect(2)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .background(Color.black.opacity(0.4))
+            }
         }
-        .padding(20)
+        .sheet(isPresented: $showsResult) {
+            
+            ResultView(outputImage: outputImage, inputImage: inputImage)
+        }
     }
     
+    
+    
+    private var inputSelectPart: some View {
+        LazyVGrid(columns: [GridItem()]) {
+            ForEach(Array($selectedList.enumerated()), id: \.offset) {id, $isOn in
+                Toggle(isOn: $isOn) {
+                    Text(selection[id])
+//                        Image(systemName: selectionImage[id])
+                }
+                .toggleStyle(CustomToggleStyle())
+            }
+        }
+        .frame(width: 80)
+        .background(Color(0xe4aadf).opacity(0.6))
+        .cornerRadius(8)
+        .padding(.bottom, 20)
+    }
     private var inputImageView: some View {
         Image(uiImage: inputImage)
             .resizable()
@@ -60,29 +153,66 @@ struct ModifierSelectScreen: View {
             .frame(width: SIZE, height: SIZE)
     }
     
-    private var outputImageView: some View {
-        VStack {
-            
-            if let outputImage = outputImage {
-                Image(uiImage: outputImage)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: SIZE, height: SIZE)
-            } else {
-                VStack {
-                    ProgressView()
-                        .progressViewStyle(CircularProgressViewStyle())
-                        .scaleEffect(2)
-                }
-                .frame(width: SIZE, height: SIZE)
-                .edgesIgnoringSafeArea(.all)
+//    private var outputImageView: some View {
+//        VStack {
+//
+//            if let outputImage = outputImage {
+//                Image(uiImage: outputImage)
+//                    .resizable()
+//                    .scaledToFit()
+//                    .frame(width: SIZE, height: SIZE)
+//            } else {
+//                VStack {
+//                    if processing {
+//                        ProgressView()
+//                            .progressViewStyle(CircularProgressViewStyle())
+//                            .scaleEffect(2)
+//                    } else {
+//                        EmptyView()
+//                    }
+//                }
+//                .frame(width: SIZE, height: SIZE)
+//                .background(Color.black.opacity(0.5))
+//                .edgesIgnoringSafeArea(.all)
+//            }
+//        }
+//    }
+    
+    private var actionButton: some View {
+        Button {
+            process()
+        } label: {
+            Text("Process")
+        }
+        .buttonStyle(.borderedProminent)
+        
+    }
+    
+    private func process() {
+        processing = true
+        outputImage = nil
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            processing = false
+            showsResult = true
+        }
+        
+        var selectedPart: [String] = []
+        selectedList.enumerated().forEach { idx, isSelected in
+            if isSelected {
+//                selectedPart.append("\(test[idx])")
+                selectedPart.append("\(idx + 1)")
             }
+        }
+        
+        ImageAPI.uploadImage(
+            image: inputImage,
+            description: inputDescription,
+            targetDescription: outputDescription,
+            parts: selectedPart.joined(separator: ",")
+        ) { image in
+            processing = false
+            outputImage = image
         }
     }
 }
-
-//struct ModifierSelectScreen_Previews: PreviewProvider {
-//    static var previews: some View {
-//        ModifierSelectScreen()
-//    }
-//}
---
+++ FacePartsEditor/view/ResultView.swift
@@ -0,0 +1,46 @@
+//
+//  ResultView.swift
+//  FacePartsEditor
+//
+//  Created by Tran Cao Khanh Ngoc on 15/06/2023.
+//
+
+import SwiftUI
+
+struct ResultView: View {
+    var outputImage: UIImage?
+    var inputImage: UIImage
+    
+    var body: some View {
+        VStack {
+            VStack {
+                if let outputImage = outputImage {
+                    Image(uiImage: outputImage)
+                } else {
+                    ProgressView()
+                        .progressViewStyle(CircularProgressViewStyle())
+                        .scaleEffect(2)
+                }
+            }
+            .frame(height: 400)
+            .frame(maxWidth: .infinity, alignment: .center)
+            .background(outputImage == nil ? Color.black.opacity(0.4) : Color.clear)
+            .padding()
+            
+            Spacer()
+
+            HStack {
+                Spacer()
+                Image(uiImage: inputImage)
+                    .resizable()
+                    .frame(width: 200, height: 200)
+                    .scaledToFit()
+            }
+            .frame(maxWidth: .infinity, alignment: .bottom)
+            .padding()
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+        .padding()
+    }
+    
+}
